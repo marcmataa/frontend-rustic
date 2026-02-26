ac9e5cf07c10ed17e59d363246a5508d176c6184
Marc <mmataanguita@gmail.com>
2026-02-26 11:45:17 +0100

    edit-carta, connect with back CRUD

--- src/app/components/edit-carta/edit-carta.component.ts
+++ src/app/components/edit-carta/edit-carta.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+// Importaciones
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import api from '../../interceptors/axios'
+import { EditCartaService } from '../../services/edit-carta.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-carta',
@@ -9,5 +11,39 @@ import api from '../../interceptors/axios'
   styleUrl: './edit-carta.component.css',
 })
 export class EditCartaComponent {
-  private cartaService = inject(CartaService)
+  // El inject lo que hace es decir dame lo que tengas de EditCartaservice y te lo doy
+  private editCartaService = inject(EditCartaService);
+  router=inject(Router)
+  // Creo un acceso signal del servicio para si algo cambia, aqui tambien
+  editCarta= this.editCartaService.editCartaSignal
+  
+  // El funcionamiento de ngOnInit es buscar y ejecutar automaticamente una vez el componente en el navegador.
+  async ngOnInit(){
+try {
+
+  if(!localStorage.getItem('token')){
+    this.router.navigate(['/login']);
+    return;
+  }
+  // Lo que hace esto es cojer la informacion que llega del back y con un get hace que aparezcan en el HTML
+  await this.editCartaService.getCarta()
+  console.log(this.editCarta())
+} catch (e){
+  console.log(e)
+}
+}
+async createDishes(){
+  const newDishes = {
+    name: 'Nombre Producto',
+    description: 'Descripcion Producto',
+    price: '10',
+    category: 'burguers'
+  };
+}
+async updateDishes(dishes: any){
+  console.log('Actualizando: ', dishes)
+}
+async deleteDishes(id: string){
+  console.log('Eliminando plato: ', id)
+}
 }
--- src/app/services/edit-carta.service.ts
+++ src/app/services/edit-carta.service.ts
@@ -1,28 +1,46 @@
 import { Injectable, signal } from '@angular/core';
-import api from '../interceptors/axios'
+import api from '../interceptors/axios';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EditCartaService {
   // Creamos un signal con un array vacio y en editCartaSignal es donde se guardaran los platos de la BD
-  editCartaSignal= signal<any[]>([])
+  editCartaSignal = signal<any[]>([]);
 
-  async getCarta(){
+  async getCarta() {
     // Este config es para decir a axios la informacion que llevara el objeto
     const config = {
-      method: 'post',
-      url:'carta/edit-carta',
-      data:''
-    }
+      method: 'get',
+      url: 'carta/',
+      data: '',
+    };
     try {
       const response = await api.request(config);
       // Esto sirve para recibir los datos que tiene el response.data
-      this.editCartaSignal.set(response.data);
-      console.log('Datos en servicio: ', this.editCartaSignal())
-    } catch(e){
+      this.editCartaSignal.set(response.data.data);
+      console.log('Datos en servicio: ', this.editCartaSignal());
+    } catch (e) {
       console.log('Error en el servicio: ', e);
       throw e;
     }
-  };
+  }
+  async updateDish(id: String, updatedData: any) {
+    const config = {
+      method: 'put',
+      url: `carta/update/${id}`,
+      data: updatedData,
+    };
+    try {
+      const response = await api.request(config);
+      // Ponemos el this.getCarta() para que Signal se actualice automaticamente una vez hagas la modificacion, es decir se vea la tabla de mongoDB correctamente
+      await this.getCarta();
+      console.log('Datos en servicio de editar: ', this.editCartaSignal());
+      // Cuando hacemos algun cambio enviamos este return para que angular sepa si la accion en el back tuvo algun fallo o funcionó correctamente
+      return response.data;
+    } catch (e) {
+      console.log('Error en el servicio: ', e);
+      throw e;
+    }
+  }
 }
