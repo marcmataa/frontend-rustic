78d65cea13848c4852b0471a81f9e330a9f1e2db
Marc <mmataanguita@gmail.com>
2026-02-27 12:25:25 +0100

    solucionado lo de tener que recargar la pagina para las cosas de los roles

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,28 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import api from '../../interceptors/axios';
 import { FormsModule } from '@angular/forms';
-import { AdminService } from '../../services/admin.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [FormsModule, CommonModule], //FormsModule se necesita para poder usar el ngModel en el HTML y asi poder leer lo que escribes
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  // Con el contructor podemos traer la variable AdminService que hemos creado en los services para asi poder usar el isAdmin en la aplicación.
-  constructor(
-    private admin: AdminService,
-    private router: Router,
-  ) {}
+ 
+private authService = inject(AuthService);
+  private router = inject(Router);
+ 
   email = signal('');
   password = signal('');
   // El onSubmit sirve para cuando se le da la accion de entrar con el boton
-  onSubmit() {
+  onLogin() {
     let data = JSON.stringify({
-      //Sirve para convertir el objeto en un a cadena de texto en formato JSON
       email: this.email(),
       password: this.password(),
     });
@@ -41,25 +40,21 @@ export class LoginComponent {
       .request(config)
       .then((response) => {
         console.log('Usuario logueado con exito');
-        const UserData = response.data;
+      
 
-        console.log('Datos recibidos del Back:', UserData);
+        console.log('Datos recibidos del Back:', response.data);
 
-        const token = UserData.data.token;
-        const userRole = UserData.data.user.role;
+        const token = response.data.data.token;
+        const role = response.data.data.user.role;
         // Si el usuario tiene token decimos que isAdmin es true y almacenamos el token en el localStorage
         if (token) {
-          this.admin.isAdmin.set(userRole === 'admin');
-          localStorage.setItem('token', token);
-          localStorage.setItem('role', userRole);
+     this.authService.loginSuccess(token, role);
           console.log('Token guardado: ', token);
+          console.log('Rol: ', role)
 
           this.email.set('');
-          this.password.set('');
-
-          console.log(UserData.data.user.role);
-          // El window location lo que hace es que cuando inicies sesion recargue la pagina y la envie a la ruta que le damos, porque si no iciera esa recarga, al entrar con el token false y al loguearte siendo admin, te saltaria un error porque no daria tiempo a cargar
-          window.location.href = '/';
+          this.password.set('')
+          this.router.navigate(['/']);
         }
       })
       .catch((error) => {
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
   private router = inject(Router);
-
+  private isAdminSignal = signal<boolean>(localStorage.getItem('role') === 'admin');
   private checkLogginStatus(): boolean {
     const token = localStorage.getItem('token');
     if (!token) {
@@ -20,11 +20,20 @@ export class AuthService {
 
   // Al ser publico se puede leer el valor desde cualquier sitio, y con el asReadonly, lo que se hace es que se pueda ver, pero no modificar el valor.
   public isLogged = this.isLoggedSignal.asReadonly();
+  public isAdmin = this.isAdminSignal.asReadonly();
 
+  loginSuccess(token: string, role: string) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('role', role);
+
+    this.isLoggedSignal.set(true);
+    this.isAdminSignal.set(role === 'admin'); // <--- Esto activará la reactividad en el HTML
+  }
   logout() {
     localStorage.removeItem('token');
     localStorage.removeItem('role');
     this.isLoggedSignal.set(false);
+    this.isAdminSignal.set(false);
     this.router.navigate(['/']);
   }
 }
